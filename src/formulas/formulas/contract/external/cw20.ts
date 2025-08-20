5e2465fb6bea8ec042e406eb11ec237ef95351bb
Noah Saso <6721426+NoahSaso@users.noreply.github.com>
2025-08-20 17:05:08 -0400

    throw error on cw20 balance not found

--- src/formulas/formulas/contract/external/cw20.ts
+++ src/formulas/formulas/contract/external/cw20.ts
@@ -70,11 +70,13 @@ export const balance: ContractFormula<string, { address: string }> = {
     )
     const addressData = isLegacy ? fromBech32(address).data : address
 
-    return (
-      (await get<string>(contractAddress, 'balance', addressData))?.valueJson ??
-      // If no balance is found, return 0.
-      '0'
-    )
+    const balance = (await get<string>(contractAddress, 'balance', addressData))
+      ?.valueJson
+    if (!balance) {
+      throw new Error(`no balance found for ${address}`)
+    }
+
+    return balance
   },
 }
 
